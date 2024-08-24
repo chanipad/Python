b41373c1376004d7f2c93498f1a176d4bc307f25
chanipad <chanipa.dencharoen@gmail.com>
2024-08-25 00:22:15 +0200

    Update new project to git

---
+++ bonus/bonus3.py
@@ -0,0 +1,13 @@
+
+meals = ['pasta', 'pizza', 'salad']
+
+for meals in meals:
+    print(meals.capitalize())
+
+
+print("Bye")
+print("----------------------------------\n")
+print("----------------------------------\n")
+
+for char in 'meals':
+    print(char.capitalize())
